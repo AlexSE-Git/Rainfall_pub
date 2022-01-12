10ab440fdaa786452a7145020fb8fd6439009775
Alex <alexsepublic@outlook.com>
2022-01-12 06:49:09 +0000

    passed parallel test

--- demo.sh
+++ demo.sh
@@ -3,12 +3,21 @@
 echo "Using sequential simulator"
 ./rainfall_seq 1 30 0.25 128 artifacts/sample_128x128.in
 python3 ./check.py 128 artifacts/sample_128x128.out output.txt
-rm output.txt
-sleep 1
 ./rainfall_seq 1 20 0.5 16 artifacts/sample_16x16.in
 python3 ./check.py 16 artifacts/sample_16x16.out output.txt
-rm output.txt
-sleep 1
 ./rainfall_seq 1 10 0.25 4 artifacts/sample_4x4.in
 python3 ./check.py 4 artifacts/sample_4x4.out output.txt
+echo "-------"
+
+echo "Using parallel"
+for i in 1 2 4 8
+do
+    echo "THREAD = $i"
+    ./rainfall_pt $i 30 0.25 128 artifacts/sample_128x128.in
+    python3 ./check.py 128 artifacts/sample_128x128.out output.txt
+    ./rainfall_pt $i 20 0.5 16 artifacts/sample_16x16.in
+    python3 ./check.py 16 artifacts/sample_16x16.out output.txt
+    ./rainfall_pt $i 10 0.25 4 artifacts/sample_4x4.in
+    python3 ./check.py 4 artifacts/sample_4x4.out output.txt
+done
 echo "-------"
